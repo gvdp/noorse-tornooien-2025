890bc8f9b36d2d2eae2213018e5bfebcc4cab80a
gvdp <vdputteglenn@gmail.com>
2025-04-08 22:20:38 +0200

    chore: set first sheet config

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 import { SheetConfig } from "sheets-as-an-api-functions";
 
 export const SHEET_IDS: SheetConfig = {
-  SHEETNAME: {
-    sheetID: '',
-    name: '',
+  U17: {
+    sheetID: '1NucBQdBle-GZVcWpXh7FeKuSdGAuX37GTNYZvdQ9l9E',
+    name: 'Jongens U17',
   }
 }
