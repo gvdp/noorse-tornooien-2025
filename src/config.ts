43f00a275c42779d823f2d9eb74573077dd5f356
gvdp <vdputteglenn@gmail.com>
2025-04-09 23:13:31 +0200

    chore: add all boys categories

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,15 @@ export const SHEET_IDS: SheetConfig = {
   U17: {
     sheetID: '1NucBQdBle-GZVcWpXh7FeKuSdGAuX37GTNYZvdQ9l9E',
     name: 'Jongens U17',
-  }
+  },
+
+U6: {sheetID: "1jDfAjN1g0IxSlg8U1m7IWv0rj9dngppsqpIcwv_3w_E", name: "Jongens U6"},
+U7: {sheetID: "1WKSnLcIXNy1PagmsPI9Yz0ROPrVXYzYW1X9GjIdl6Gs", name: "Jongens U7"},
+U8: {sheetID: "16Jopz11uzup8j0Yq2EIwvfeRA2W8GRAEI8FbI56hi6E", name: "Jongens U8"},
+U9: {sheetID: "1YcntrEnnIhg1dKWBeaPwGBXvmQro63dh8lnXzP5MRyE", name: "Jongens U9"},
+U15: {sheetID: "1XAczwuX-8ib2n8FKmqQdERjOQ9LbBvtapecb1IKfreg", name: "Jongens U15"},
+U10: {sheetID: "11tHgTKRxcbkVVrKn9pg4llGFCoN58P2q8y28vdWcWsU", name: "Jongens U10"},
+U11: {sheetID: "1mfJvhhUk9rMEyTI8ZXuYudc83CplbejTLvnQGxPqN2Q", name: "Jongens U11"},
+U12: {sheetID: "1wgE5oMr7YJ5pVXJTtlpViisqFrEMmrETb-uakKvTMwo", name: "Jongens U12"},
+U13: {sheetID: "1ZUtDgW52yUr4wqTfUgqI6Qg3c9_iok0W4FYpQOHRfys", name: "Jongens U13"},
 }
